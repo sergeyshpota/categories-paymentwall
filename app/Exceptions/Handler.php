9ecadc96ec30cae4b29f1b694cbbac06602e2521
Sergey Shpota <sergey@neversettle.it>
2020-11-15 21:56:37 +0200

    Handle 404 and 405 responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -33,5 +37,27 @@ class Handler extends ExceptionHandler
     public function register()
     {
         //
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Throwable $e
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response|void
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($request->wantsJson()) {
+            if ($e instanceof MethodNotAllowedHttpException) {
+                return response()->json(['message' => 'Method not allowed', 'errors' => []], 405);
+            } else if ($e instanceof NotFoundHttpException) {
+                return response()->json(['message' => 'Route not found', 'errors' => []], 404);
+            } else if ($e instanceof ModelNotFoundException) {
+                return response()->json(['message' => 'Resource not found', 'errors' => []], 404);
+            }
+        }
+
+        return parent::render($request, $e);
     }
 }
